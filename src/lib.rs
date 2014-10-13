ed1d39c8a601fe6a1768c5e69127f56ff75ffdf4
Kevin Butler <haqkrs@gmail.com>
2014-10-13 07:03:12 +0100

    fix(connection_pool): fix depreciations by using r2d2_postgres
    BREAKING CHANGE
    
    A handler needs to be supplied to `new`, you can use
    r2d2::NoopErrorHandler to keep the same behaviour as before.
    
    Code calling `get_connection` will change to `get` and should be OK.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 extern crate nickel;
 extern crate postgres;
+extern crate r2d2;
+extern crate r2d2_postgres;
 
 pub use middleware::{ PostgresMiddleware, PostgresRequestExtensions };
 
--- src/middleware.rs
+++ src/middleware.rs
@@ -1,33 +1,47 @@
 use nickel::{ Request, Response, Middleware, Action, Continue, NickelError };
-use postgres::pool::{ PooledPostgresConnection, PostgresConnectionPool };
-use postgres;
+use postgres::{PostgresConnection, SslMode};
+use r2d2_postgres::{PostgresPoolManager, Error};
+use r2d2::{Pool, ErrorHandler, Config, NoopErrorHandler, PooledConnection};
+use std::default::Default;
+use std::sync::Arc;
 
-#[deriving(Clone)]
-pub struct PostgresMiddleware {
-    pub pool: PostgresConnectionPool
+pub struct PostgresMiddleware<H: ErrorHandler<Error>> {
+    pub pool: Arc<Pool<PostgresConnection, Error, PostgresPoolManager, H>>
 }
 
-impl PostgresMiddleware {
-    pub fn new (connect_str: &str, sslMode: postgres::SslMode, num_connections: uint) -> PostgresMiddleware {
+impl<H> PostgresMiddleware<H> where H: ErrorHandler<Error> {
+    pub fn new(connect_str: &str, sslMode: SslMode, num_connections: uint, handler: H)
+            -> PostgresMiddleware<H> {
+        let manager = PostgresPoolManager::new(connect_str, sslMode);
+        let config = Config {
+            pool_size: num_connections,
+            ..Default::default()
+        };
+
         PostgresMiddleware {
-            pool: PostgresConnectionPool::new(connect_str, sslMode, num_connections).unwrap()
+            pool: Arc::new(Pool::new(config, manager, handler).unwrap())
         }
     }
 }
 
-impl Middleware for PostgresMiddleware {
+impl<H> Middleware for PostgresMiddleware<H> where H: ErrorHandler<Error> {
     fn invoke (&self, req: &mut Request, _resp: &mut Response) -> Result<Action, NickelError> {
-        req.map.insert(self.pool.clone().get_connection());
+        req.map.insert(self.pool.clone());
         Ok(Continue)
     }
 }
 
 pub trait PostgresRequestExtensions {
-    fn db_conn(&self) -> &PooledPostgresConnection;
+    fn db_conn(&self) -> PooledConnection<PostgresConnection, Error, PostgresPoolManager, NoopErrorHandler>;
 }
 
 impl<'a, 'b> PostgresRequestExtensions for Request<'a, 'b> {
-    fn db_conn(&self) -> &PooledPostgresConnection {
-        return self.map.find::<PooledPostgresConnection>().unwrap();
+    fn db_conn(&self) -> PooledConnection<PostgresConnection, Error, PostgresPoolManager, NoopErrorHandler> {
+        self.map.find::<Arc<Pool<PostgresConnection,
+                             Error,
+                             PostgresPoolManager,
+                             NoopErrorHandler>>>().unwrap()
+                                                 .get()
+                                                 .unwrap()
     }
 }
