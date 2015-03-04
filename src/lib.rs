0c5253c69e3aae67db997161ade749af65901929
Kevin Butler <haqkrs@gmail.com>
2015-03-04 21:42:06 +0000

    fix(*): align with current nickel

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ extern crate nickel;
 extern crate postgres;
 extern crate r2d2;
 extern crate r2d2_postgres;
+extern crate plugin;
+extern crate typemap;
 
 pub use middleware::{ PostgresMiddleware, PostgresRequestExtensions };
 
